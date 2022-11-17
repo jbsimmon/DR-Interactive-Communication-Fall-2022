07d24d34da214e61c019adfd2fc281c5da8307d8
tenkka <106789081+tenkka@users.noreply.github.com>
2022-11-16 16:10:43 -0800

    Update animal.py

--- animal.py
+++ animal.py
@@ -5,16 +5,12 @@ from flask import Flask
 import tensorflow_hub as hub
 from sklearn.metrics.pairwise import cosine_similarity
 
-# print("load wv")
-# wv = api.load('word2vec-google-news-300')
-# print("wv loaded")
-
 API_TOKEN = "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"
 
 API_URL = "https://api-inference.huggingface.co/models/vblagoje/bert-english-uncased-finetuned-pos"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-module_path = "universal-sentence-encoder_4"
+module_path = "F:/universal-sentence-encoder_4"
 sim_model = hub.load(module_path)
 
 def query(payload):
@@ -29,11 +25,9 @@ def create_obj(obj, adj):
     global OBJ_ID
     global OBJ_LIST
     go = {"Name": obj + str(OBJ_ID), "Type": obj, "Location": [0, 2*OBJ_ID, 0], "Color": "default", "Size": "default"}
-    for i in adj:
-        if i in ["red", "blue", "green", "white"]:
-            go["Color"] = i
-        if i in ["big", "medium", "small"]:
-            go["Size"] = i
+    go["Location"] = adj["Location"]
+    go["Color"] = adj["Color"]
+    go["Size"] = adj["Size"]
     OBJ_LIST.append(go)
     OBJ_ID += 1
     return go
@@ -51,13 +45,13 @@ def find_adj(part):
     colors = ["black", "blue", "cyan", "gray", "green", "magenta", "red", "white", "yellow"]
     sizes = ["small tiny mini", "medium standard normal", "large huge big"]
     locations = ["far", "close", "front", "back", "right", "left", "high", "low"]
-    adj = {"Color": None, "Size": None, "Location": [0, 0, 0]}
+    adj = {"Color": None, "Size": None, "Location": [0, 0, 0]} 
     c = [part] + colors
     embeddings = sim_model(c)
     similarity = cosine_similarity(embeddings, embeddings)[0][1:]
     mv = max(similarity)
     if mv > 0.1:
-        mi = similarity.index(mv)
+        mi = list(similarity).index(mv)
         adj["Color"] = colors[mi]
 
     s = [part] + sizes
@@ -65,7 +59,7 @@ def find_adj(part):
     similarity = cosine_similarity(embeddings, embeddings)[0][1:]
     mv = max(similarity)
     if mv > 0.1:
-        mi = similarity.index(mv)
+        mi = list(similarity).index(mv)
         adj["Size"] = sizes[mi].split()[0]
 
     l = [part] + locations
@@ -131,7 +125,7 @@ def process(text):
                 similarity = cosine_similarity(embeddings, embeddings)[0][1:]
                 mv = max(similarity)
                 if mv > 0.1:
-                    mi = similarity.index(mv)
+                    mi = list(similarity).index(mv)
                     op = OP[mi]
             if word["entity_group"] == "NOUN":
                 l = [word["word"], "chicken", "cow", "duck", "pig", "sheep"]
@@ -139,11 +133,11 @@ def process(text):
                 similarity = cosine_similarity(embeddings, embeddings)[0][1:]
                 mv = max(similarity)
                 if mv > 0.1:
-                    mi = similarity.index(mv)
+                    mi = list(similarity).index(mv)
                     ob = TYPE[mi]
 
         if ob and op == "create":
-            adj = find_adj(p)
+            adj = find_adj(" ".join([i["word"] for i in p]))
             create_list.append(create_obj(ob, adj))
             pre = op
         elif ob and op == "delete":
@@ -153,7 +147,7 @@ def process(text):
                     delete_list.append(i)
             pre = op
         elif ob and pre == "create":
-            adj = find_adj(p)
+            adj = find_adj(" ".join([i["word"] for i in p]))
             create_list.append(create_obj(ob, adj))
         elif ob and pre == "delete":
             result = delete_obj(ob, adj)
